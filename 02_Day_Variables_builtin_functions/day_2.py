3dcd4838dfcc80d4f2d8baad0ce95aacaec1a24f
BeanieDuck <wyattjc006@proton.me>
2026-03-27 11:15:37 -0400

    Update day_2.py

--- 02_Day_Variables_builtin_functions/day_2.py
+++ 02_Day_Variables_builtin_functions/day_2.py
@@ -1,1 +1,50 @@
+print("Day 2: 30 Days of programming")
+first_name = input("What is your first name? ")
+last_name = input ("What is your last name? ")
+full_name = 'wyatt_carroll'
+country = input("What is your country ")
+city = 'chambersburg'
+age = int(input("What is your age? "))
+year = 2026
+is_married = False
+is_true = True
+is_light_on = True
+interests = ['Tech', 'Environment', 'Science']
+person_info = {'firstname': 'Wyatt', 'lastname': 'carroll', 'country': 'usa', 'city': 'chambersburg'}
 
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(interests))
+print(type(person_info))
+
+print(len(first_name))
+print(len(last_name))
+
+num_one = 5
+num_two = 4
+
+a = (num_one - num_two)
+b = (num_two - num_one)
+c = (num_one * num_two)
+d = (num_one / num_two)
+e = (num_one % num_two)
+f = (num_one ** num_two)
+g = (num_one // num_two)
+
+import math
+radius = (int(input("enter radius ")))
+area_of_circle = math.pi * radius ** 2
+circumference = 2 * math.pi * radius
+
+(print(area_of_circle))
+
+print("Your name is:", first_name, last_name)
+print("You are", age, "years old.", "You are from", country)
